9c6768079dd90e671575b4769711610387f6b275
Daniel <danielwarnecke1daniel@gmail.com>
2021-05-27 17:48:55 +0200

    Nachladen

--- PLayer/Player_f.gd
+++ PLayer/Player_f.gd
@@ -6,6 +6,8 @@ var velocity = Vector2()
 
 var bullet =preload("res://PLayer/bullet.tscn")
 
+var can_shoot = true
+
 func _process(delta):
 	velocity.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	velocity.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
@@ -14,6 +16,10 @@ func _process(delta):
 	
 	global_position += speed * velocity * delta
 	
-	if Input.is_action_just_pressed("click") and Global.node_creation_parent != null:
+	if Input.is_action_pressed("click") and Global.node_creation_parent != null and can_shoot:
 		Global.instance_node(bullet, global_position, Global.node_creation_parent)
-	
+		$Reload_speed.start()
+		can_shoot = false
+
+func _on_Reload_speed_timeout():
+	can_shoot = true
