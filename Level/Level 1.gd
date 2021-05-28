a9556f8dd7a1b07946655e3efabc7330e054cf17
Daniel <danielwarnecke1daniel@gmail.com>
2021-05-28 12:33:32 +0200

    enemy spawning verbessert

--- Level/Level 1.gd
+++ Level/Level 1.gd
@@ -14,7 +14,7 @@ func _exit_tree():
 func _on_Enemy_spawn_timer_timeout():
 	var enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
 	
-	while enemy_position.x < 640 and enemy_position.x > -80 or enemy_position.y < 360 and enemy_position.y > -45:
+	while enemy_position.x < 640 and enemy_position.x > -80 and enemy_position.y < 360 and enemy_position.y > -45:
 		enemy_position = Vector2(rand_range(-160, 670), rand_range(-90, 390))
 		
 	Global.instance_node(enemy_1, enemy_position, self)
