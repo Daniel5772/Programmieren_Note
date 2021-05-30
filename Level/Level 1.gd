92971998f2205280b9da5547c302cee7f4c5e013
Daniel <danielwarnecke1daniel@gmail.com>
2021-05-30 14:54:15 +0200

    bug fixes

--- Level/Level 1.gd
+++ Level/Level 1.gd
@@ -27,7 +27,7 @@ func _on_Difficulty_timer_timeout():
 
 
 func _on_Difficulty_timer_2_timeout():
-	if $Enemy_spawn_timer_2.wait_time > 3:
+	if $Enemy_spawn_timer_2.wait_time > 4:
 		$Enemy_spawn_timer_2.wait_time -= 0.1
 
 
