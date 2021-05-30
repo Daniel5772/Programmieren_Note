29ed6668bdde92a83f846856eda5eb3bf39d14d9
Daniel <danielwarnecke1daniel@gmail.com>
2021-05-30 10:16:56 +0200

    small bug fixed

--- Welten/Finished.gd
+++ Welten/Finished.gd
@@ -6,10 +6,11 @@ func _on_Menu_button_pressed():
 	get_tree().change_scene("res://Screne/Menu.tscn")
 	Global.player_hp = 5
 	Global.score = 0
+	Global.is_in_world = false
 
 
 func _on_Worldsbutton_pressed():
 	get_tree().change_scene("res://Welten/World hub.tscn")
 	Global.player_hp = 5
 	Global.score = 0
-
+	Global.is_in_world = false
